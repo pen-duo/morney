7ce354f8fcd0d9f5b0960806b0761a4afac9965b
pen-duo <2411363896@qq.com>
2021-02-13 15:37:26 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import Home from '../views/Home.vue'
 import Money from "@/views/Money.vue"
 import Labels from "@/views/Labels.vue"
 import Statistics from "@/views/Statistics.vue"
+import NotFound from "@/views/NotFound.vue"
+
 
 Vue.use(VueRouter)
 
@@ -20,6 +22,10 @@ const routes: Array<RouteConfig> = [
   }, {
     path: "/statistics",
     component: Statistics
+  },
+  {
+    path:"*",
+    component: NotFound
   }
 ]
 
