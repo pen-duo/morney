db88050a43af9546efc0be8f13973f419b6abfda
pen-duo <2411363896@qq.com>
2021-02-19 21:26:27 +0800

    新增EditLabel组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -3,7 +3,25 @@
 </template>
 
 <script>
-export default {};
+import tagListModel from "@/models/tagListModel";
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class EditLabel extends Vue {
+  created() {
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    console.log(tags);
+    const tag = tags.filter((item) => item.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace("/404");
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
